927df150561d98b94b83b5c0247bb0110454f7fc
Jason Patterson <jp3477@columbia.edu>
2017-11-19 13:04:04 -0500

    fixed warnings which count as errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MailingList from './app/components/MailingList';
 import { Modal } from 'semantic-ui-react';
